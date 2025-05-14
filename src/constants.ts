68e24b34f1e82a3276abf2c53edd07a235601d77
Collin Campbell <collincampbell97@proton.me>
2025-05-14 17:47:17 -0400

    Removed Base Files and Updated Values

--- src/constants.ts
+++ src/constants.ts
@@ -18,26 +18,20 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
+    name: "Github",
+    href: "https://github.com/Relicjamin-jv",
+    linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/campbell-collin/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:collincampbell97@proton.me",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
---
+++ src/data/blog/calculus/index.md
@@ -0,0 +1,16 @@
+---
+title: Calculus
+author: Collin Campbell
+pubDatetime: 2025-05-14
+slug: calc-index
+featured: false
+draft: false
+hideEditPost: true
+tags:
+  - calculus
+description: Calculus Index
+---
+
+## Index
+
+...
